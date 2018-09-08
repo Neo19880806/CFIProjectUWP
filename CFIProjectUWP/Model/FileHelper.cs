9f2f8995ff9370648ec0ea1281991bcfec781fc5
Neo <245891559@qq.com>
2018-09-08 22:36:56 +0930

    Clean Project

---
+++ CFIProjectUWP/Model/FileHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Windows.Storage;
+
+namespace CFIProjectUWP.Model
+{
+    public class FileHelper
+    {
+        public static async Task saveObjectToXML<T>(String fileName,T saveObject)
+        {
+            var serializer = new XmlSerializer(typeof(T));
+            var folder = ApplicationData.Current.LocalFolder;
+            var file = await folder.CreateFileAsync(fileName, CreationCollisionOption.ReplaceExisting);
+            var stream = await file.OpenStreamForWriteAsync();
+            using (stream)
+            {
+                serializer.Serialize(stream, saveObject);
+            }
+        }
+
+        public static async Task<T> readObjectFromXML<T>(String fileName)
+        {
+            T objectFromXML = default(T);
+            var serializer = new XmlSerializer(typeof(T));
+
+            var folder = ApplicationData.Current.LocalFolder;
+            var file = await folder.GetFileAsync(fileName);
+            var stream = await file.OpenStreamForReadAsync();
+            using (stream)
+            {
+                objectFromXML = (T)serializer.Deserialize(stream);
+            }
+            return objectFromXML;
+        }
+
+        public static async Task<bool> IsFileExist(String fileName)
+        {
+            bool fileExists = false;
+            var allfiles = await ApplicationData.Current.LocalFolder.GetFilesAsync();
+            foreach (var storageFile in allfiles)
+            {
+                if (storageFile.Name == fileName)
+                {
+                    fileExists = true;
+                }
+            }
+            return fileExists;
+        }
+    }
+}
